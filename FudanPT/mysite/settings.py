7185f5ef021dc7a60967e5313cea49566b9e727c
神火 <dollarser@126.com>
2021-02-25 20:25:12 +0800

    完善静态资源

--- FudanPT/mysite/settings.py
+++ FudanPT/mysite/settings.py
@@ -117,5 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 STATIC_URL = '/static/'
